6bcbad251169ddb5025f2de6074e22d09432819e
Adrian <adi78900@gmail.com>
2023-10-16 11:32:20 +0200

    Add game models.

---
+++ src/main/java/com/example/gameserver/model/domain/Genre.java
@@ -0,0 +1,37 @@
+package com.example.gameserver.model.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "genre")
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "game_genre",
+            joinColumns = @JoinColumn(name = "genre_id"),
+            inverseJoinColumns = @JoinColumn(name = "game_id"))
+    private Set<Game> games = new LinkedHashSet<>();
+
+}
