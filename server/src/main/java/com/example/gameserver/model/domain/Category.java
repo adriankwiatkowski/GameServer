bcc9c4d4f2ed2c80ded28c915a83ddf4fb69c709
Adrian <adi78900@gmail.com>
2023-10-21 11:20:26 +0200

    Remove @JsonIgnore as we no longer return Game object.

--- server/src/main/java/com/example/gameserver/model/domain/Category.java
+++ server/src/main/java/com/example/gameserver/model/domain/Category.java
@@ -1,6 +1,5 @@
 package com.example.gameserver.model.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -29,7 +28,6 @@ public class Category {
     @Column(name = "name", nullable = false, unique = true)
     private String name;
 
-    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "game_category",
             joinColumns = @JoinColumn(name = "category_id"),
--- server/src/main/java/com/example/gameserver/model/domain/Platform.java
+++ server/src/main/java/com/example/gameserver/model/domain/Platform.java
@@ -1,6 +1,5 @@
 package com.example.gameserver.model.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -29,7 +28,6 @@ public class Platform {
     @Column(name = "name", nullable = false, unique = true)
     private String name;
 
-    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "game_platform",
             joinColumns = @JoinColumn(name = "platform_id"),
